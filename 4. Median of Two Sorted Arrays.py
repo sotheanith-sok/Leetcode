ffcb553928e91b3f646c5c20303e34f2989d2c09
Sotheanith Sok <Sotheanith.Sok@outlook.com>
2022-05-30 23:02:00 -0700

    Update 4

--- 4. Median of Two Sorted Arrays.py
+++ 4. Median of Two Sorted Arrays.py
@@ -15,11 +15,11 @@ Problem:
     Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.
 
 Solution (Binary Search):
-    To find the median, we will perform a binary search on the small array and its mid point will be used to determine the small array left partition and the large array left partition such that the sum of their lengths will be the same as the merged array left partition. Let m and n be the midpoint of the small and large arrays respectively. If we pick the correct m, small[m] < large[n+1] and large[n] < small[m+1]. If small[m] > large[n+1], it means that we picked too many values from the small array and thus, the correct m will on the left partition. If large[n] > small[m+1], it means that we picked too little values from the small array and thus, the correct m will be on the right partition. Repeat the process until the correct m is found.   
+    To find the median, we will perform a binary search on the small array and its mid point will be used to determine the small array left partition(including the midpoint) and the large array left partition such that the sum of their lengths will be the same as the merged array left partition. Let m and n be the midpoint of the small and large arrays respectively. If we pick the correct m, small[m] <= large[n+1] and large[n] <= small[m+1]. If small[m] > large[n+1], it means that we picked too many values from the small array and thus, the correct m will on the left partition. If large[n] > small[m+1], it means that we picked too little values from the small array and thus, the correct m will be on the right partition. Repeat the process until the correct m is found. Assume negative infinity and positive infinity for out of bound.   
 
 Complexity:
-    Time:
-    Space
+    Time: O(log(min(m,n)))
+    Space: O(1)
 """
 
 
@@ -28,29 +28,45 @@ class Solution:
 
         # Initialze variables.
         small, large = nums1, nums2
-        m, n, half = 0, 0, (len(small) + len(large))//2
+        m, n = 0, 0
+        total = len(small) + len(large)
+        half = total // 2
 
         # Determine which of the given array is smaller.
         if len(small) > len(large):
             small, large = large, small
 
         # Pointers for the binary search.
-        left, right = 0, len(small)-1
+        left, right = 0, len(small) - 1
 
         while True:
 
             # Calculate midpoints of both arrays.
             m = (left + right) // 2
-            n = half - m - 1
+            n = half - m - 2
 
-            if small[m] <= large[n+1] and large[n] <= small[m+1]:
-                return 
+            # Assume negative and positive infinity when out of bound
+            small_m = small[m] if m >= 0 else float("-infinity")
+            small_m_next = small[m + 1] if (m + 1) < len(small) else float("infinity")
+            large_n = large[n] if n >= 0 else float("-infinity")
+            large_n_next = large[n + 1] if (n + 1) < len(large) else float("infinity")
 
-            elif small[m] > large[n+1]:
-                right = m
+            # Found the mid point
+            if small_m <= large_n_next and large_n <= small_m_next:
+
+                # Even case.
+                if total % 2 == 0:
+                    return (max(large_n, small_m) + min(large_n_next, small_m_next)) / 2
+
+                # Odd case.
+                else:
+                    return min(large_n_next, small_m_next)
+
+            # Overpick from the small array, the midpoint is somewhere left.
+            elif small_m > large_n_next:
+                right = m - 1
+
+            # Underpick from the small array, the midpoint is somewhere right.
             else:
-                left = m
+                left = m + 1
 
-
-print(Solution().findMedianSortedArrays(
-    [1, 2, 3, 4, 5, 6, 7, 8], [1, 2, 3, 4, 5]))
