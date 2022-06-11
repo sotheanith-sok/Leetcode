3abd2fb5bc46fa93eee081988dcb7e812da20ce3
Sotheanith Sok <Sotheanith.Sok@outlook.com>
2022-06-10 17:16:46 -0700

    Create 150. Evaluate Reverse Polish Notation.py

---
+++ 150. Evaluate Reverse Polish Notation.py
@@ -0,0 +1,71 @@
+""" 
+Problem:
+    Evaluate the value of an arithmetic expression in Reverse Polish Notation.
+
+    Valid operators are +, -, *, and /. Each operand may be an integer or another expression.
+
+    Note that division between two integers should truncate toward zero.
+
+    It is guaranteed that the given RPN expression is always valid. That means the expression would always evaluate to a result, and there will not be any division by zero operation.
+
+    Example 1:
+    Input: tokens = ["2","1","+","3","*"]
+    Output: 9
+    Explanation: ((2 + 1) * 3) = 9
+    
+    Example 2:
+    Input: tokens = ["4","13","5","/","+"]
+    Output: 6
+    Explanation: (4 + (13 / 5)) = 6
+    Example 3:
+    Input: tokens = ["10","6","9","3","+","-11","*","/","*","17","+","5","+"]
+    Output: 22
+    Explanation: ((10 * (6 / ((9 + 3) * -11))) + 17) + 5
+    = ((10 * (6 / (12 * -11))) + 17) + 5
+    = ((10 * (6 / -132)) + 17) + 5
+    = ((10 * 0) + 17) + 5
+    = (0 + 17) + 5
+    = 17 + 5
+    = 22
+
+Solution:
+    We will use a stack to store all numbers and the partial result. For any operation, the first number will be the second value from the end of the stack and the second number will be the last value in the stack.
+
+Complexity:
+    Time: O(n)
+    Space: O(n)
+
+"""
+
+
+class Solution:
+    def evalRPN(self, tokens: list[str]) -> int:
+
+        # Stack to store numbers and partial result
+        stack = []
+
+        # Iterate through all tokens
+        for t in tokens:
+
+            # Adding
+            if t == "+":
+                stack.append(stack.pop(-2) + stack.pop())
+            
+            # Subtracting
+            elif t == "-":
+                stack.append(stack.pop(-2) - stack.pop())
+            
+            # Multiplying
+            elif t == "*":
+                stack.append(stack.pop(-2) * stack.pop())
+            
+            # Dividing
+            elif t == "/":
+                stack.append(int(stack.pop(-2) / stack.pop()))
+            
+            # If it is a number, just add it to the stack.
+            else:
+                stack.append(int(t))
+
+        return stack[-1]
+
