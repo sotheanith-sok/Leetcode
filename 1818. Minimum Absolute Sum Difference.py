3afe0a9fbc7622f4dfb387ee47faba842d29c9e9
Sotheanith Sok <Sotheanith.Sok@outlook.com>
2022-06-23 17:38:50 -0700

    Update 1818. Minimum Absolute Sum Difference.py

--- 1818. Minimum Absolute Sum Difference.py
+++ 1818. Minimum Absolute Sum Difference.py
@@ -34,50 +34,75 @@ Problem:
     This yields an absolute sum difference of |10-9| + |10-3| + |4-5| + |4-1| + |2-7| + |7-4| = 20
 
 Solution:
+    We will iterate through all pair of numbers in nums1 and nums2. At each iteration, we calculate the absolute difference and add it to the total. This represent the difference if we don't replace a value. Then, we use a binary search to find a number in nums1 that is closest to the number in nums2 and calculate their absolute difference. This represent the difference if we replace a value with a better number. Keep track of the best replacement aka the largest difference between the non-replace difference and replace difference. Lastly, return the new total by subtracting the best non-replace difference and adding the best replace difference.   
 
 Complexity:
-    Time:
-    Space:
+    Time: O(nlogn)
+    Space: O(n)
 """
+
+
+from math import inf
 
 
 class Solution:
     def minAbsoluteSumDiff(self, nums1: list[int], nums2: list[int]) -> int:
+        # Create a new nums1 and sort it
         sorted_nums1 = list(nums1)
         sorted_nums1.sort()
 
+        # Binary search to find a value in nums1 cloest to t
         def binarySearch(t):
-            l, r, res = 0, len(sorted_nums1) - 1, None
 
+            # Initialize the left pointer, the right pointer, the result variable.
+            l, r, res = 0, len(sorted_nums1) - 1, inf
+
+            # Repeat the search until the left and right pointers crossed.
             while l <= r:
+
+                # Calculate the mid pointer
                 m = (r + l) // 2
 
+                # Calculate the difference between the target and the value at mid pointer
                 diff = t - sorted_nums1[m]
-                old_diff = t - res if res != None else diff * 2
 
-                if abs(diff) <= abs(old_diff):
+                # If the value at mid pointer is closer to the target and the previous result, update the result
+                if abs(diff) <= abs(t - res):
                     res = sorted_nums1[m]
 
+                # If the difference is 0, we have found the cloest possible value and thus, we can end the search
                 if diff == 0:
                     break
+
+                # If the difference is larger than 0, there could be a value to the right that is closer to target
                 elif diff > 0:
                     l = m + 1
+
+                # If the difference is smaller than 0, there could be a value to the left that is closer to the target.
                 else:
                     r = m - 1
+
             return res
 
-        total = 0
-        max_saved = [0, 0]
+        # Initialize varaibles to keep track of total, best difference if not replace, and best difference if replaced.
+        total, bestNotReplace, bestReplace = 0, 0, 0
 
-        for i, v in enumerate(nums2):
-            oldDiff = abs(nums1[i] - nums2[i])
-            total += oldDiff
+        # Iterate through all pairs in nums1 and nums2
+        for i in range(len(nums2)):
 
-            newDiff = abs(binarySearch(v) - nums2[i])
-            if max_saved[0] - max_saved[1] < oldDiff - newDiff:
-                max_saved = [oldDiff, newDiff]
+            # Calculate the difference if we don't replace
+            notReplace = abs(nums1[i] - nums2[i])
 
-        return (total - max_saved[0] + max_saved[1]) % 1000000007
+            # Calculate the difference if we replaced
+            replace = abs(binarySearch(nums2[i]) - nums2[i])
 
-print(Solution().minAbsoluteSumDiff(nums1=[1, 7, 5], nums2=[2, 3, 5]))
+            # Add the not replace difference to the total
+            total += notReplace
+
+            # If we found a better saving than previously (aka the gap between not replace difference and replace difference is larger), update corresponding varaibles
+            if bestNotReplace - bestReplace < notReplace - replace:
+                bestNotReplace, bestReplace = notReplace, replace
+
+        # Return the new total mod 10^9+7
+        return (total - bestNotReplace + bestReplace) % 1000000007
 
