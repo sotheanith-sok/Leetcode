c4a5dd2e0507dbb6039ba1203f4d1cc65d0fbd69
Sotheanith Sok <Sotheanith.Sok@outlook.com>
2022-08-06 20:53:22 -0700

    Update 220. Count Vowels Permutation.py

--- 220. Count Vowels Permutation.py
+++ 220. Count Vowels Permutation.py
@@ -80,10 +80,11 @@ class Solution:
     def countVowelPermutation(self, n: int) -> int:
 
         # Initialize all vowels counts to 1
+        # This is n==1 case
         a, e, i, o, u = 1, 1, 1, 1, 1
 
-        # Iterate from 1 to n
-        for _ in range(1, n):
+        # Iterate from 2 to n
+        for _ in range(2, n+1):
             a, e, i, o, u = e + i + u, a + i, e + o, i, i + o
 
         # Return the sum of all counts of all vowels
