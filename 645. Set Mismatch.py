373b700f564f5a0173281258e26f25359f4e7049
Sotheanith Sok <Sotheanith.Sok@outlook.com>
2022-10-22 18:30:07 -0700

    modified:   645. Set Mismatch.py

--- 645. Set Mismatch.py
+++ 645. Set Mismatch.py
@@ -15,33 +15,36 @@ Problem:
     Output: [1,2]
 
 Solution:
-    Count all numbers and their occruences. Then, iterate thorugh all numbers from 1 to n and find a number with frequency of 2 and a number with frequency of 1. Return both numbers.
+    Iterate through all numbers and mark it as seen by using such number as an index and change a number at such index to negative. Then, if we see a number that seen before, we have found a solution. Lastly, iterate through nums one more time and find the index where the number isn't negative. This is the other solution. 
 
 Complexity:
     Time: O(n)
-    Space: O(n)
+    Space: O(1)
 """
-
-
-from collections import Counter
 
 
 class Solution:
     def findErrorNums(self, nums: list[int]) -> list[int]:
 
-        # Initialize length of nums, result, counter
-        n, res, counts = len(nums), [0, 0], Counter(nums)
+        # Initialize length of nums and result
+        n, res = len(nums), [0, 0]
 
-        # Iterate through all numbers from 1 to n
-        for num in range(1, n + 1):
+        # Iterate through all numbers
+        for num in nums:
 
-            # If the current number has 2 occurence, save it into the result
-            if counts[num] == 2:
+            # Get the current number
+            num = abs(num)
+
+            # If we have see the current number before, we have found a solution
+            if nums[num - 1] < 0:
                 res[0] = num
+                continue
 
-            # If the current number has 0 occurnece, save it into the result
-            if counts[num] == 0:
-                res[1] = num
+            # Mark the current number as seen by changing the number at the corresponding index to negative
+            nums[num - 1] = -nums[num - 1]
+
+        # Find the number that we havn't see before
+        res[1] = [i + 1 for i in range(n) if nums[i] > 0][0]
 
         return res
 
