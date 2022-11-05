8af60344771b58b325688ea4fc6b13dba974ccba
Sotheanith Sok <Sotheanith.Sok@outlook.com>
2022-11-04 20:47:33 -0700

    modified:   212. Word Search II.py

--- 212. Word Search II.py
+++ 212. Word Search II.py
@@ -13,120 +13,103 @@ Problem:
     Output: []
 
 Solution:
-    Instead of searching for each word by peforming dfs on each character, we will use a Trie or Prefix Tree to solve this problem. A trie is tree contains linked nodes where each node is a character and form chains that represent words. This data structure enables us to efficiently searching for words that have the same prefix. To start with, we will iterate through the board until we find the character that is a children of the root node. Then, we start our DFS through the tree from there until we reach a node that has been marked as the end of a word. Then, we append the word to the result. Continue until we reach a deadend and backtrack. For each iteration, a next node is determine by unvisited neighboring characters given in the board. 
-    
-    Note: We can reduce the number of the node to search through in a trie by removing any visited node that doesn't have any children aka all of its children have been visited.  
+    We will store all words in a trie tree so we can search for all words at once and we will use the board as an adjacency list. Iterate through all entries in the board and use it as the starting node. For each starting node, dfs through the trie tree and if we can reach a leaf, we have found a word. 
 
+    We can prune the trie tree by recursively removing all visited nodes starting from the leaf.
+
+    Unlike normal dfs, we will only mark a board entiry as visited when we actually visit it as the next entry can contribute to multiple words.
+
+    Ex:    l a b   word = ['baca','back']
+           k c a    
 Complexity:
-    Time: m n 4**(m n)
-    Space: O(m n)
+    Time: mn 4**(mn)
+    Space: O(mn)
 """
 
-# The trie node
-class TrieNode:
-    def __init__(self) -> None:
+from collections import defaultdict
+from itertools import product
 
-        # A hashmap mapping char -> trie node
-        self.children = {}
+# Defintion of the trie tree
+class Trie:
+    def __init__(self):
 
-        # Mark the end of a word
+        # Initialize a dict to store all child trie nodes
+        self.children = defaultdict(lambda: Trie())
+
+        # A flag to mark if the current node is the end of a word
         self.isWord = False
 
     def add(self, word):
 
-        # Start with root aka itself
+        # Initialize the current node
         node = self
 
-        # For each char in a word
-        for char in word:
+        # Iterate through all characters in a word 
+        for c in word:
 
-            # If it isn't in the hashamp
-            if char not in node.children:
+            # Go to the next node
+            node = node.children[c]
 
-                # Add it
-                node.children[char] = TrieNode()
-
-            # Set the current node to that child
-            node = node.children[char]
-
-        # Lastly, mark this node as the end of the word
+        # Mark the last node as the end of a word
         node.isWord = True
 
 
 class Solution:
     def findWords(self, board: list[list[str]], words: list[str]) -> list[str]:
 
-        # Create a trie tree
-        root = TrieNode()
-
-        # Add all words to the trie tree
+        # Initialize a trie and add all words into it
+        trie = Trie()
         for word in words:
-            root.add(word)
+            trie.add(word)
 
-        # Create sets to store res and visited nodes
-        res, visit = set(), set()
+        # Find length of rows and cols
+        m, n = len(board), len(board[0])
+
+        # Initialize sets of result and visited nodes
+        res, visited = set(), set()
 
         # A helper function for directional movement
-        direction = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
 
-        # DFS to search through the trie tree
-        def dfs(row, col, word, node, parentNode=None):
+        # DFS through the trie tree
+        def dfs(row, col, word, node, pNode):
 
-            # End the search if
-            #   A given row and col aren't valid or
-            #   This row and col already visited or
-            #   The character at the given row and col isn't a child of the current node.
+            # If the current node is out of bound or it is already visited or the character isn't in the trie tree, end the search
             if (
-                row < 0
-                or row >= ROWS
-                or col < 0
-                or col >= COLS
-                or (row, col) in visit
+                not (0 <= row < m)
+                or not (0 <= col < n)
+                or (row, col) in visited
                 or board[row][col] not in node.children
             ):
                 return
 
-            # Save the current node a parent node
-            parentNode = node
+            # Update the current node and parent node
+            node, pNode = node.children[board[row][col]], node
 
-            # Update the current node to the child node.
-            node = node.children[board[row][col]]
-
-            # Append the character to the word
+            # Add the current character onto the word
             word += board[row][col]
 
-            # If the current node is the end of a word
+            # If we have reached the end of a word, add it into the result
             if node.isWord:
-
-                # Add the word to the result
                 res.add(word)
 
-            # Mark the current row and col as visited
-            visit.add((row, col))
+            # Mark the current node as visited
+            visited.add((row, col))
 
-            # DFS call on its neighbors
-            for dRow, dCol in direction:
-                neiRow, neiCol = row + dRow, col + dCol
+            # Visit next nodes
+            for dRow, dCol in directions:
+                nRow, nCol = row + dRow, col + dCol
+                dfs(nRow, nCol, word, node, pNode)
 
-                dfs(
-                    neiRow, neiCol, word, node, parentNode,
-                )
+            # Unmark the current node as visited
+            visited.remove((row, col))
 
-            # Mark the current row and col as unvisited
-            visit.remove((row, col))
+            # Prune the trie tree and remove all visited node from the leaf
+            if pNode and not node.children:
+                pNode.children.pop(board[row][col])
 
-            # Remove the current node if it doesn't have any children as we already visited it
-            # This will reduce the number of nodes in the trie tree
-            if parentNode and len(node.children) == 0:
-                parentNode.children.pop(board[row][col])
+        # Iterate through all entires in the board and use it as the starting node
+        for row, col in product(range(m), range(n)):
+            dfs(row, col, "", trie, None)
 
-        # Get the rows and cols
-        ROWS, COLS = len(board), len(board[0])
-
-        # Perform DFS on all characters in the board
-        for i in range(ROWS):
-            for j in range(COLS):
-                dfs(i, j, "", root)
-
-        return list(res)
-
+        return res
