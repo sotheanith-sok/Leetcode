79a917e944c16d433845f5f2166dfd8001b290b8
Sotheanith Sok <Sotheanith.Sok@outlook.com>
2022-12-14 22:03:46 -0800

    modified:   1143. Longest Common Subsequence.py

--- 1143. Longest Common Subsequence.py
+++ 1143. Longest Common Subsequence.py
@@ -39,28 +39,25 @@ Complexity:
 """
 
 
+from itertools import product
+
+
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
 
         # Get length of text1 and text2
-        m = len(text1) + 1
-        n = len(text2) + 1
+        m, n = len(text1) + 1, len(text2) + 1
 
         # Create cache to stores sub problems
         cache = [[0 for _ in range(n)] for _ in range(m)]
 
         # Fill in the cache
-        for i in range(m)[1:]:
-            for j in range(n)[1:]:
-                cache[i][j] = (
-                    cache[i - 1][j - 1]
-                    + 1  # Look diagonal and add one if text1[m] == text2[n]
-                    if text1[i - 1] == text2[j - 1]
-                    else max(
-                        cache[i - 1][j], cache[i][j - 1]
-                    )  # max of top and left entry
-                )
+        for i, j in product(range(1, m), range(1, n)):
+            cache[i][j] = (
+                cache[i - 1][j - 1]
+                + 1  # Look diagonal and add one if text1[m] == text2[n]
+                if text1[i - 1] == text2[j - 1]
+                else max(cache[i - 1][j], cache[i][j - 1])  # max of top and left entry
+            )
 
         return cache[-1][-1]
-
-
