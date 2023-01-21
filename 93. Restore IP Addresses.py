76c75e3eb8064204a7ae667077ff545b4e84fbc2
Sotheanith Sok <sotheanith.sok@outlook.com>
2023-01-21 10:10:35 -0800

    modified:   93. Restore IP Addresses.py

--- 93. Restore IP Addresses.py
+++ 93. Restore IP Addresses.py
@@ -18,40 +18,60 @@ Problem:
     Output: ["1.0.10.23","1.0.102.3","10.1.0.23","10.10.2.3","101.0.2.3"]
 
 Solution:
-    We can solve this problem with backtracking. At each iteration, we extract 1 to 3 digits from the string and append a dot at the end of it. The remaining string will be its own sub problems. We know that we found a valid ip address if we used exactly 4 dots and we reach the end of the string. Else, if we use more than 4 dots or the potential digits create value bigger than 255 or it has a leading zero, we stop and backtrack.    
+    We can solve this problem with backtracking. At each iteration, we will pick the next term for a proposed ip address as long as the term is valid. We know that we have a valid ip if we used all digits and we picked 4 terms and thus, we can append the proposed ip into the result. Then, we backtrack.  
 
 Complexity:
-    Time: 3**n 
-    Space: 3**n
+    Time: O(3**n) 
+    Space: O(3**n)
 
 """
 
 
 class Solution:
     def restoreIpAddresses(self, s: str) -> list[str]:
-        # Since ip address is ground into 4 group of 3 digits, we couldn't generate valid ips from a string longer than 12 characters.
-        if len(s) > 12:
+
+        # Find the number of digits
+        n = len(s)
+
+        # If we have too little or too many digits to form a valid ip, end the search
+        if not (4 <= n <= 12):
             return []
 
+        # Initialize the result
         res = []
 
-        def backtrack(i, dots, ip):
-            # If we used 4 dots and reach the end of the string, we found a valid ip.
-            # 122.555.666.777. -> 122.555.666.777
-            if dots == 4 and i == len(s):
-                res.append(ip[:-1])
+        # Recursively forming valid ips
+        def backtrack(i, ip):
 
-            # If we use more than 4 dots, we won't be able to form a valid ip address
-            if dots > 4:
+            # If we used all digits or we have 4 terms, we end the search
+            if i == n or len(ip) == 4:
+
+                # If both conditions hold true, we have found a valid ip
+                if i == n and len(ip) == 4:
+                    res.append(".".join(ip))
+
                 return
 
-            # Recursive call for the three sub problems. min is used to prevent out of bound.
-            for j in range(i, min(i + 3, len(s))):
+            # Else, we will pick the next term
+            for j in range(i + 1, min(i + 4, n + 1)):
 
-                # Only generate a sub problem if there is no leading zero and the potential digits are less than or equal to 255. i==j ensures that a single 0 digit will be accepted.
-                if int(s[i : j + 1]) <= 255 and (i == j or s[i] != "0"):
-                    backtrack(j + 1, dots + 1, ip + s[i : j + 1] + ".")
+                # If the next term is greater than 255, end the search
+                if int(s[i:j]) > 255:
+                    return
 
-        backtrack(0, 0, "")
+                # Else, add the term into the ip
+                ip.append(s[i:j])
+
+                # Go to the next term
+                backtrack(j, ip)
+
+                # Once we return, pop the picked term from the ip
+                ip.pop()
+
+                # If there is leading 0, we will end the search after picked the first possible term
+                if s[i] == "0":
+                    return
+
+        backtrack(0, [])
+
         return res
-
